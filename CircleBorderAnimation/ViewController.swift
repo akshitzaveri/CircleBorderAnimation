8cfd3ed00994f20dc19850171248ad2269a7cb7b
Akshit Zaveri <akshit.zaveri@gmail.com>
2020-05-02 15:32:24 +0530

    Initial Commit and setup

---
+++ CircleBorderAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircleBorderAnimation
+//
+//  Created by Akshit Zaveri on 02/05/20.
+//  Copyright © 2020 Akshit Zaveri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var circleView: UIView!
+  @IBOutlet weak var button: UIButton!
+
+  @IBAction func buttonTapped(_ sender: UIButton) {
+    
+  }
+}
